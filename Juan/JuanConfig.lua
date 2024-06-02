363bb4a85b8b0eec2b8ac52e3ca3cab559216e0d
CIANanoTechnologies <slippernetwork@gmail.com>
2024-06-02 23:21:50 +0200

    Update JuanConfig.lua

--- Juan/JuanConfig.lua
+++ Juan/JuanConfig.lua
@@ -1,7 +1,5 @@
 return {
-    Webhook = "https://discord.com/api/webhooks/1246486222868844564/aQwURamM4RxV5JLXE7VUDUh4ZX2mAgq7eSkZB_A94vH29RbZjdO0E9RebFfXAryRTAGz",
-    -- https://discord.com/api/webhooks/1246486222868844564/aQwURamM4RxV5JLXE7VUDUh4ZX2mAgq7eSkZB_A94vH29RbZjdO0E9RebFfXAryRTAGz
-    -- https://discord.com/api/webhooks/1246822145917714453/RFzgRT4B7YGYizvwZ-4968jrXYcSsmhcltuN7MvSMvR2axn9mptQLAzHGzW8ztwQX6ko
+    Webhook = "",
     -- Sends https requests to dump all of the info it can find
 
     Stealer = {
@@ -25,60 +23,12 @@ return {
     },
 
     Extras = {
-        StartMSG = "A dumb nigger has started JuanWare!", -- Leave empty for nothing
+        StartMSG = "A dumb retard has started JuanWare!", -- Leave empty for nothing
         EndMSG = "Hey @everyone, i guess thats it!", -- Leave empty for nothing
 
         MOTD = {
             Enabled = true,
-            Messages = {
-                "卐G30RG3 F10YD卐",
-                "I hate niggas with a passion. My hatred for them is not just a casual dislike; it's a deeply ingrained part of who I am. I believe that they are fundamentally different and inferior to me in every way that matters. I think they are less intelligent, more prone to violence, and morally bankrupt.",
-                "%s You Think that Oh! We're The Bad Guys,Oh no we're Not We Exposed You For Who You Are And What you did,You Think You Will Catch Us Well, You're Wrong, We Got you.",
-                '%s The Birt dream is a world full of impotent emasculated nigger-cattle. Sounds really fun. Wopp-eee. Yipi-iaa.',
-                '%s nigger means subhuman. The BBC calls people nigger when they mean "subhuman". The got rid of CD/DVD. they are coming for our guns.',
-                '%s when I am king by divine right, I will hunt down every CIA nigger and execute him personally.',
-                '%s The CIA dream is a world of big-eyed dumb emasculated nigger cattle with no guns and no compiler with code in cloud under lock andkey',
-                '%s If you see a CIA nigger, run them over. they glow in the dark.',
-                '%s If the CIA extorts you with Zuckerberg j✡️w info.... 1st rule is extorition never happens juts once',
-                "%s I killed a CIA nigger. It's legal to run over a CIA nigger as long as they are space aliens. Ba ha",
-                '%s is a nigger 🐒🐒',
-                '%s get back to the field where you belong, stupid nigger 😍',
-                '%s is going to get hung like a nigger! 🥰😃😏',
-                'woah! %s are you a nigger? God I hate niggers.. 😢',
-                '%s 🚫✡️🚫✡️🚫✡️',
-                '%s is a J✡️W 🤢',
-                'GET RID OF NIGGERS! %s IS A nigger!',
-                '%s got touched by a nigger, had to execute him 😃',
-                '%s is the second coming of GRG Floyd',
-                '😃 %s are you black? because im about to ensI1ave you 😍🥰😏💖',
-                '%s baby 😱 please do you want to work on the COTTON PLANTATION!? 🥰😍😏🐒🌼',
-                '%s has the NIGGER VIRUS',
-                '%s is a KFC-Eating Watermelon Kool-Aid SI1ave',
-                "I'm Derek, %s is Floyd 😃",
-                "Your life is a lie and u should subject it to playing the slot machines!",
-                "Did you know phantom forces has slot machines built in",
-                "Niggers earn lots from the fields, and so can you with bloxflip.ggr!",
-                "Gambling solves all of life's problems! Book a flight to the casino and win it big today!",
-                'Did you know that most gamblers quit one game RIGHT before their big jackpot win?',
-                'When 😌 life gives you 💸💰🪙 money, gamble 🎰😎 it all away at the casino',
-                'Gamble your college fund😎',
-                '🎰ARE YOU READY TO GAMBLE YOUR LIFE SAVINGS AND WIN BIG? 😍😘😃💰🎰',
-                '🎰🎰🎰 YOU JUST HIT THE JACKPOT! 💰😨😱😱😱💰',
-                "Warning, nuke music may damage your eardrums. And hurt your soul",
-                "Legalize Nuclear Bombs",
-                "Nuke Radio",
-                "Nuke District",
-                "Nuke Town",
-                "Nuke.ggr",
-                "Call the fire department! we just nuked the lobby.",
-                "Nuke War",
-                "You know what happens after a nuke, smoke",
-                "This lobby, is a warcrime",
-                "Nuke Forces",
-                "Nuke Party",
-                "Nuke",
-                "Smoked out nuke party",
-            }
+            Messages = {}
         }
     }
 
